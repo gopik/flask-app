dca9ea97a1fc0db26c980e762769ca690d05afb4
Gopi Madabhushi <gopik@users.noreply.github.com>
2016-04-27 10:18:29 +0530

    GET /order/<orderid>

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'hello_world from flask'
+@app.route('/order/<order_id>')
+def get_order(order_id):
+	if order_id='0x1234567812345678':
+		return jsonify({
+			order_id: '0x1234567812345678'
+			mid: 10,
+			tid: 100,
+			amount: 2304
+			})
+	else:
+		return jsonify({
+			order_id: '0x1234567812345678'
+		})
 
 if __name__ == '__main__':
 	app.run()
